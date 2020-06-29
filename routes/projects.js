a7c014190f7c9c8114e4db9a3a77cf5adefd1347
mikiteek <mikiteek@gmail.com>
2020-06-29 16:33:10 +0300

    add 'project-edit.hbs' and rewrite rout 'projects.js' with edit

--- routes/projects.js
+++ routes/projects.js
@@ -20,4 +20,32 @@ router.get("/:id" ,async (req, res) => {
   });
 });
 
+router.get("/:id/edit", async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect("/");
+  }
+  const project = await Project.findById(req.params.id);
+  res.render("project-edit", {
+    title: `Edit ${project.title}`,
+    project
+  });
+});
+
+router.post("/edit", async (req, res) => {
+  const {id} = req.body;
+  delete req.body.id;
+  await Project.findByIdAndUpdate(id, req.body);
+  res.redirect("/projects");
+});
+
+router.post('/remove', async (req, res) => {
+  try {
+    await Project.deleteOne({_id: req.body.id});
+    res.redirect("/project");
+  }
+  catch (e) {
+    console.log(e)
+  }
+});
+
 module.exports = router;
